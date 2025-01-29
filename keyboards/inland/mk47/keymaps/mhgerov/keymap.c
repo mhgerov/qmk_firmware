3b09cba5a8e5761339421fb1611006cfe43113be
Michael Gerov <mhgerov@gmail.com>
2025-01-29 04:53:53 -0500

    add mod keys on lower layer

--- keyboards/inland/mk47/keymaps/mhgerov/keymap.c
+++ keyboards/inland/mk47/keymaps/mhgerov/keymap.c
@@ -27,17 +27,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [1] = LAYOUT_planck_mit(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DOT,
-        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_F11,  KC_F12,  KC_VOLU,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,    MO(3),   KC_MPRV, KC_MPLY, KC_MFFD, KC_VOLD
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,           KC_F9,           KC_F10,          KC_TRNS,
+        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,            KC_9,            KC_0,            KC_DOT,
+        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,         KC_F11,          KC_F12,          RSFT_T(KC_VOLU),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,    MO(3),   RGUI_T(KC_MPRV), RALT_T(KC_MPLY), RCTL_T(KC_MFFD), KC_VOLD
     ),
 
     [2] = LAYOUT_planck_mit(
-        KC_TILD,         KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO, KC_NO,   KC_MINS, KC_LBRC, KC_RBRC, KC_NO,   KC_TRNS,
-        CW_TOGG,         KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_DQUO, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS, KC_PIPE,
-        LSFT_T(KC_CAPS), KC_NO,   KC_NO,   KC_NO,   KC_V,    KC_NO, KC_NO,   KC_EQL,  KC_LPRN, KC_RPRN, KC_NO,   KC_NO,
-        KC_NO,           KC_TRNS, KC_TRNS, KC_TRNS, MO(3),       KC_SPC,     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
+        KC_TILD,         KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_NO, KC_NO,   KC_DQUO, KC_LBRC, KC_RBRC, KC_NO,   KC_TRNS,
+        CW_TOGG,         KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_PMNS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS, KC_PIPE,
+        LSFT_T(KC_CAPS), KC_NO,   KC_NO,   KC_NO,   KC_V,     KC_NO, KC_UNDS, KC_EQL,  KC_LPRN, KC_RPRN, KC_NO,   KC_NO,
+        KC_NO,           KC_TRNS, KC_TRNS, KC_TRNS, MO(3),       KC_SPC,      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
     )
 
 };
