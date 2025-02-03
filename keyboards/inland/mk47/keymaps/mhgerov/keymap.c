268e54c9293288f63e999015f3bab1d1697ba126
Michael Gerov <mhgerov@gmail.com>
2025-02-03 06:14:09 -0500

    add adjust layer with d-pad

--- keyboards/inland/mk47/keymaps/mhgerov/keymap.c
+++ keyboards/inland/mk47/keymaps/mhgerov/keymap.c
@@ -23,21 +23,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,            KC_O,            KC_P,            KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,            KC_SCLN,         KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,         KC_DOT,          KC_SLSH,         RSFT_T(KC_QUOT),
-        KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, MO(1),       KC_SPC,       MO(2),   RGUI_T(KC_LEFT), RALT_T(KC_DOWN), RCTL_T(KC_UP),   KC_RGHT
+        KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, TL_LOWR,     KC_SPC,       TL_UPPR, RGUI_T(KC_LEFT), RALT_T(KC_DOWN), RCTL_T(KC_UP),   KC_RGHT
     ),
 
     [1] = LAYOUT_planck_mit(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,           KC_F9,           KC_F10,          KC_TRNS,
         KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,            KC_9,            KC_0,            KC_DOT,
         KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,         KC_F11,          KC_F12,          RSFT_T(KC_VOLU),
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,    MO(3),   RGUI_T(KC_MPRV), RALT_T(KC_MPLY), RCTL_T(KC_MFFD), KC_VOLD
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,    KC_TRNS, RGUI_T(KC_MPRV), RALT_T(KC_MPLY), RCTL_T(KC_MFFD), KC_VOLD
     ),
 
     [2] = LAYOUT_planck_mit(
-        KC_TILD,         KC_NO,   KC_LBRC, KC_RBRC, KC_DQUO, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_RBRC, KC_NO,   KC_TRNS,
-        CW_TOGG,         KC_NO,   KC_LCBR, KC_RCBR, KC_PLUS, KC_PMNS, KC_NO,  KC_NO,   KC_NO,   KC_RCBR, KC_BSLS, KC_PIPE,
-        LSFT_T(KC_CAPS), KC_NO,   KC_LPRN, KC_RPRN, KC_EQL,  KC_UNDS, KC_NO,  KC_NO,   KC_NO,   KC_RPRN, KC_NO,   KC_RSFT,
-        KC_NO,           KC_TRNS, KC_TRNS, KC_TRNS, MO(3),        KC_SPC,     KC_TRNS, KC_RGUI, KC_RALT, KC_RCTL, KC_NO
+        KC_TILD,         KC_NO,   KC_LBRC, KC_RBRC, KC_DQUO, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+        CW_TOGG,         KC_NO,   KC_LCBR, KC_RCBR, KC_PLUS, KC_PMNS, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_BSLS, KC_PIPE,
+        LSFT_T(KC_CAPS), KC_NO,   KC_LPRN, KC_RPRN, KC_EQL,  KC_UNDS, KC_NO,  KC_NO,   KC_LABK, KC_RABK, KC_NO,   KC_RSFT,
+        KC_NO,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_SPC,     KC_TRNS, KC_RGUI, KC_RALT, KC_RCTL, KC_NO
+    ),
+
+    [3] = LAYOUT_planck_mit(
+        KC_NO, KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_NO, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
 // clang-format on
